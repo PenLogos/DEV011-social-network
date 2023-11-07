42087f9a80e20bfa379f8be6e6959dbceb18ecc7
PenLogos <142206877+PenLogos@users.noreply.github.com>
2023-11-07 12:09:30 -0500

    arreglo feed 7

--- src/components/feed.js
+++ src/components/feed.js
@@ -4,7 +4,7 @@ import {
 } from '../lib/database';
 import LogoPeque from '../images/LogoPeque.png';
 
-export const renderPostContainer = (renderTextPost, renderDisplayName, postId, renderPhoto, dataLikes = []) => {
+export const renderPostContainer = (renderTextPost, renderDisplayName, postId, dataLikes = [], renderPhoto) => {
   const postContainer = document.createElement('section');
   postContainer.classList.add('post-square');
   const postContainerPage = `
